a3c14fa45690a41d5f5df53cdf84e69afe164d8b
MateuszMyszke <myszke93@gmail.com>
2023-08-23 18:38:30 +0200

    Fix error server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
@@ -7,12 +8,21 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [ProductsModule, PrismaModule, ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'client', 'build'),
-  }), UsersModule, AuthModule],
+  }), UsersModule, AuthModule, ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,11 +7,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-  imports: [UsersModule, PassportModule,  JwtModule.registerAsync({
+  providers: [AuthService, LocalStrategy, JwtStrategy, UsersService],
+  imports: [ ConfigModule, UsersModule, PassportModule, PrismaModule, JwtModule.registerAsync({
     imports: [ConfigModule],
     useFactory: (configService: ConfigService) => ({
       secret: configService.get('jwt.secret'),
