da9cdd58a7bc97964a32dac9a716fd30fdb7ef2c
MateuszMyszke <myszke93@gmail.com>
2023-08-23 12:56:18 +0200

    Add product endponits

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule, PrismaModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client', 'build'),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Product } from './products.interface';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private prismaService: PrismaService){}
+
+  public getAll(): Promise<Product[]> {
+    return this.prismaService.product.findMany({ include: { publisher: true }})
+  }
+
+  public getById(id: Product['id']): Promise<Product | null>{
+    return this.prismaService.product.findUnique({
+      where: { id },
+      include: { publisher: true },
+    })
+  }
+}
