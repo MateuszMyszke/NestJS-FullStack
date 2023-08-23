0044701265fdfc19c1b531af09b1c685194caf9e
MateuszMyszke <myszke93@gmail.com>
2023-08-23 17:17:58 +0200

    Add users

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ProductsModule, PrismaModule, ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'client', 'build'),
-  })],
+  }), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
