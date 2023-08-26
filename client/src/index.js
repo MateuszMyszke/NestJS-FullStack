f8a98d732e6602f55d25c4c5fcddeff28bec0313
MateuszMyszke <myszke93@gmail.com>
2023-08-26 17:21:11 +0200

    Add redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 
 const Root = () => (
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider> 
   </Router>
 ); 
 
